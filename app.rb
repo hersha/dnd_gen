8f4b3d41a62a1b883dde27af537ec70960506ab5
Patrick Neff <pneff@apple.com>
2015-10-18 17:17:43 -0700

    Moved to rack and add gitignore

--- app.rb
+++ app.rb
@@ -1,41 +1,43 @@
-require 'sinatra'
+require 'sinatra/base'
 
-CLASSES = %w(Barbarian Bard Cleric Druid Fighter Monk Paladin Ranger Rogue Sorcerer Warlock Wizard)
-RACES = %w(Dwarf Elf Halfling Human Dragonborn Gnome Half-Elf Half-Orc Tiefling Goliath Genasi)
-GENDER = %w(Male Female)
-ALIGNMENT_X = ["Lawful", "Chaotic"]
-ALIGNMENT_Y = %w(Good Neutral Evil)
+class App < Sinatra::Base
+  CLASSES = %w(Barbarian Bard Cleric Druid Fighter Monk Paladin Ranger Rogue Sorcerer Warlock Wizard)
+  RACES = %w(Dwarf Elf Halfling Human Dragonborn Gnome Half-Elf Half-Orc Tiefling Goliath Genasi)
+  GENDER = %w(Male Female)
+  ALIGNMENT_X = ["Lawful", "Chaotic"]
+  ALIGNMENT_Y = %w(Good Neutral Evil)
 
-def stat_roll
-  rolls = 4.times.map { |r| rand(6) + 1}.sort
-  sum = rolls[1..-1].reduce(:+)
-end
-
-def stats
-  @stats ||= [0]
-  until @stats.reduce(:+) > 67
-    @stats = 6.times.map { |r| stat_roll }.sort
+  def stat_roll
+	rolls = 4.times.map { |r| rand(6) + 1}.sort
+	sum = rolls[1..-1].reduce(:+)
   end
-  @stats
-end
 
-def generate
-  @stats = nil
-  @klass = CLASSES.sample
-  @race = RACES.sample
-  @gender = GENDER.sample
-  @alignment = [ALIGNMENT_X.sample, ALIGNMENT_Y.sample].compact.join(" ")
-  stats
-end
+  def stats
+	@stats ||= [0]
+	until @stats.reduce(:+) > 67
+	  @stats = 6.times.map { |r| stat_roll }.sort
+	end
+	@stats
+  end
 
-set :public_folder, 'public'
+  def generate
+	@stats = nil
+	@klass = CLASSES.sample
+	@race = RACES.sample
+	@gender = GENDER.sample
+	@alignment = [ALIGNMENT_X.sample, ALIGNMENT_Y.sample].compact.join(" ")
+	stats
+  end
 
-get '/' do
-  @stats = nil
-  @klass = CLASSES.sample
-  @race = RACES.sample
-  @gender = GENDER.sample
-  @alignment = [ALIGNMENT_X.sample, ALIGNMENT_Y.sample].compact.join(" ")
-  stats
-  erb :index
+  set :public_folder, 'public'
+
+  get '/' do
+	@stats = nil
+	@klass = CLASSES.sample
+	@race = RACES.sample
+	@gender = GENDER.sample
+	@alignment = [ALIGNMENT_X.sample, ALIGNMENT_Y.sample].compact.join(" ")
+	stats
+	erb :index
+  end
 end
